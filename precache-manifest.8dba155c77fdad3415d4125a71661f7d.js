67e06e7c875b0b8dedcb6d6ad8ac654b5403183d
Gingano <lesha.kowalenko@gmail.com>
2019-06-24 17:17:03 +0300

    Updates

---
+++ precache-manifest.8dba155c77fdad3415d4125a71661f7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394839d179405edd0e6fb8cebc209ccc",
+    "url": "/react_static_list/index.html"
+  },
+  {
+    "revision": "2dc80c539d74e0724a2a",
+    "url": "/react_static_list/static/css/main.d7abff8e.chunk.css"
+  },
+  {
+    "revision": "767e158467df4297e433",
+    "url": "/react_static_list/static/js/2.8515f2cd.chunk.js"
+  },
+  {
+    "revision": "2dc80c539d74e0724a2a",
+    "url": "/react_static_list/static/js/main.c321a756.chunk.js"
+  },
+  {
+    "revision": "13b64d2cc89a58d973eb",
+    "url": "/react_static_list/static/js/runtime~main.f5c9179e.js"
+  },
+  {
+    "revision": "6ef2b84790dbffc19d0e8bbfafbc861e",
+    "url": "/react_static_list/static/media/checked.6ef2b847.svg"
+  },
+  {
+    "revision": "40e0f797929ce7b185ed654ae67b7f13",
+    "url": "/react_static_list/static/media/unchecked.40e0f797.svg"
+  }
+]);
