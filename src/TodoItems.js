7e3862b44a393df2329cef8b6ed4281b72572488
Gingano <lesha.kowalenko@gmail.com>
2019-06-26 10:26:17 +0300

    eslint

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import todos from './api/todos';
-import users from "./api/users";
+import React from 'react'
+import todos from './api/todos'
 
 class TodoItems extends React.Component {
+  render () {
+    let currentItems = todos.filter((element) => element.userId === this.props.state.userId)
 
-    render() {
-        let currentItems = todos.filter((element) => element.userId === this.props.state.userId)
-
-        return (
-            <ul className='theList'>
-                {
-                    currentItems.map((item) =>
-                        <li
-                            key={item.id}
-                        >
-                            <button className={item.completed ? 'completed-true' : 'completed-false'}/>
-                            <label>{item.title}</label>
-                        </li>
-                    )
-                }
-            </ul>
-        );
-    }
+    return (
+      <ul className='theList'>
+        {
+          currentItems.map((item) =>
+            <li
+              key={item.id}
+            >
+              <button className={item.completed ? 'completed-true' : 'completed-false'}/>
+              <label>{item.title}</label>
+            </li>
+          )
+        }
+      </ul>
+    )
+  }
 }
 
-export default TodoItems;
+export default TodoItems
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,54 +1,51 @@
-import React from 'react';
-import TodoItems from './TodoItems';
-import todos from './api/todos';
-import users from './api/users';
+import React from 'react'
+import TodoItems from './TodoItems'
+import users from './api/users'
 import User from './User'
 
 class TodoList extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor (props) {
+    super(props)
 
-        this.state = {
-            userId: ''
-        };
-
-
+    this.state = {
+      userId: ''
     }
+  }
 
-    chooseUser = (item) => {
-        let currentUser = item.id;
-        this.setState({
-            userId: currentUser
-        });
-    };
+  chooseUser (item) {
+    let currentUser = item.id;
+    this.setState({
+      userId: currentUser
+    })
+  };
 
-    render () {
-        return (
-            <div className='toDoListMain'>
-                <ul className='select-user'>
-                    {
-                        users.map((item, index) =>
-                            <li>
-                                <button
-                                    onClick={() => this.chooseUser(item)}
-                                >
-                                    {item.name}
-                                </button>
-                            </li>
-                        )
-                    }
-                </ul>
-                <TodoItems
-                    toggleStatus={this.toggleStatus}
-                    remove={(index) => {this.remove(index)}}
-                    state={this.state}
-                />
-                <User
-                    state={this.state}
-                />
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div className='toDoListMain'>
+        <ul className='select-user'>
+          {
+            users.map((item) =>
+              <li>
+                <button
+                  onClick={() => this.chooseUser(item)}
+                >
+                  {item.name}
+                </button>
+              </li>
+            )
+          }
+        </ul>
+        <TodoItems
+          toggleStatus={this.toggleStatus}
+          remove={(index) => { this.remove(index) }}
+          state={this.state}
+        />
+        <User
+          state={this.state}
+        />
+      </div>
+    )
+  }
 }
 
-export default TodoList;
+export default TodoList
